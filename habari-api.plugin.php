94135c0fcfa605d7cdbc7572af2763dbe8ab30d7
Morgante Pell <morgante.pell@morgante.net>
2013-12-21 02:23:07 -0500

    initial commit

---
+++ habari-api.plugin.php
@@ -0,0 +1,12 @@
+<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
+
+/**
+ * CoreDashBlocks - Provides a core set of blocks for the dashboard.
+ */
+
+class HabariAPI extends Plugin
+{
+
+}
+
+?>
