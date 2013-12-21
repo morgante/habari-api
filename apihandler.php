388e418ff0d9ce1b6d192fc6d70c1c9436775cb2
Morgante Pell <morgante.pell@morgante.net>
2013-12-21 02:34:06 -0500

    add api handler

---
+++ apihandler.php
@@ -0,0 +1,24 @@
+<?php
+/**
+	* ApiHandler class
+	*
+	* @package Habari
+	*
+	* @version $Id$
+	* @copyright 2008
+ */
+
+class ApiHandler extends ActionHandler
+{
+
+	/**
+	* Respond to requests for podcast feeds
+	*
+	*/
+	public function act_base()
+	{
+		print_r('This is the API. Put something here.');
+	}
+
+}
+?>
--- habari-api.plugin.php
+++ habari-api.plugin.php
@@ -1,12 +1,44 @@
-<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
+<?php
 
-/**
- * CoreDashBlocks - Provides a core set of blocks for the dashboard.
- */
+if ( !defined( 'HABARI_PATH' ) ) {
+	die( 'No direct access' );
+}
 
-class HabariAPI extends Plugin
+require('apihandler.php');
+
+class HabariApi extends Plugin
 {
+ 		/**
+         * Add needed rewrite rules
+         */
+        public function filter_rewrite_rules( $rules )
+        {
+                $rules[] = new RewriteRule( array(
+                        'name' => 'link_feed',
+                        'parse_regex' => '%api/?$%i',
+                        'build_str' => 'api',
+                        'handler' => 'ApiHandler',
+                        'action' => 'base',
+                        'priority' => 7,
+                        'is_active' => 1,
+                        'description' => 'Displays the API base',
+                ));
 
+                // // '"link"/"redirect"/slug', 'link_redirect');                
+                
+                // $rules[] = new RewriteRule( array(
+                //         'name' => 'link_redirect',
+                //         'parse_regex' => '%link/redirect/(?P<slug>[^/]+)/?$%i',
+                //         'build_str' => 'link/redirect/{$slug}',
+                //         'handler' => 'PluginHandler',
+                //         'action' => 'link_redirect',
+                //         'priority' => 7,
+                //         'is_active' => 1,
+                //         'description' => 'Redirects to the linked item',
+                // ) );
+
+                return $rules;
+        }
 }
 
 ?>
